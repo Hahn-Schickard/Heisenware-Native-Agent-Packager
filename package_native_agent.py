da14d7ad7dc3dea820c90ae31e00d213c83869d3
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2025-10-13 17:12:29 +0200

    fix arg documentation

--- package_native_agent.py
+++ package_native_agent.py
@@ -11,21 +11,21 @@ def make_args():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
         '--input_file',
-        help='Path to the generated Heisenware native agent'
+        help='Path to the packaged Heisenware native agent binary file'
         ' binary file',
         type=str,
         required=True
     )
     parser.add_argument(
         '--output_dir',
-        help='Path to the generated Heisenware native agent'
+        help='Generated package/installer output directory (defaults to current work directory)'
         ' binary file',
         type=str,
         default=''
     )
     parser.add_argument(
         '--target_system',
-        help='Target package platform and architechture',
+        help='Target package platform and architecture',
         choices=[
             'Amd64_Debian',
             'Arm64_Debian',
@@ -35,7 +35,7 @@ def make_args():
     )
     parser.add_argument(
         '--agent_id',
-        help='Heisenware native agent id',
+        help='Generated Heisenware native agent id',
         type=str,
         required=True
     )
@@ -64,7 +64,7 @@ if __name__ == '__main__':
     args = make_args()
     package_name = f'heisenware-{args.agent_id}-{args.account_name}-{args.workspace_name}'
     this_dir = Path(Path(__file__).absolute()).parent
-    
+
     input_file = Path(args.input_file)
     if not input_file.is_file():
         print(f'Given input file {input_file} does not exist', file=sys.stderr)
